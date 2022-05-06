a1319dd4063da8a67816cb8aa5223b2d046016d7
Павел Юров <peyurov@gmail.com>
2022-05-06 13:22:00 +0300

    Добавьте файлы проекта.

---
+++ HomeWork 8 module Mission 3/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork_8_module_Mission_3
+{
+    internal class Program
+    {
+        const string Path = @"Delete";
+        static DirectoryInfo dI = new DirectoryInfo(Path);
+        static void Main(string[] args)
+        {
+         
+
+            long count = 0;
+            long height = 0;
+
+            try
+            {
+                if (Check(Path))
+                {
+                    DeletDirectory(dI,ref height,ref count);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Папка и файлы внутри удалены");
+            }
+
+
+        }
+
+        /// <summary>
+        /// Метод удалаяет необходимую нам папку
+        /// </summary>
+        static void DeletDirectory(DirectoryInfo dI,ref long height, ref long count )
+        {
+            
+
+          
+            FileInfo[] fls = dI.GetFiles();
+            
+            foreach (FileInfo f in fls)
+            {
+                count++;
+                height += f.Length;
+                f.Delete();
+                
+            }
+
+            
+
+            DirectoryInfo[] dir = dI.GetDirectories();
+
+            foreach (DirectoryInfo e in dir)
+            {
+               
+
+                FileInfo[] File = e.GetFiles();
+                
+                foreach (FileInfo f in File)
+                {
+                    count++;
+                    height += f.Length;
+                    f.Delete();
+                }
+
+
+             
+
+                e.Delete();
+
+                
+                if (dI.GetDirectories().Length == 0)
+                {
+
+                    Console.WriteLine($"Папка {dI.Name} размер: {height} \n");
+                    Console.WriteLine($"Папка {dI.Name} имела {count} файлов\n");
+                    Console.WriteLine($"Освобождено: {height}\n");
+                    
+                }
+
+               
+                DeletDirectory(dI,ref height,ref count);
+
+            }
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// Метод позволяющий проверить путь к папке,а также использование файлов в течении 30 минут
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <returns></returns>
+        static bool Check(string Path)
+        {
+            DateTime time = DateTime.Now;
+            bool check = false;
+
+
+            if (Directory.Exists(Path))
+            {
+                FileInfo FI = new FileInfo(Path);
+                DateTime timeCreate = FI.CreationTime;
+                TimeSpan result = time - timeCreate;
+
+                if (result.Minutes > 30)
+                {
+                    Console.WriteLine("Найдена директория которая неиспользовалась 30 минут");
+                    check = true;
+                }
+                else
+                {
+                    Console.WriteLine("Файлы использовализь в течении 30 минут");
+                    check = false;
+
+                }
+            }
+
+            else
+                Console.WriteLine("Проверьте указанный путь или присутствие папки");
+
+            return check;
+        }
+    }
+}    
+
---
+++ HomeWork 8 module Mission 3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeWork 8 module Mission 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWork 8 module Mission 3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f92a4548-8b95-4a76-839d-97eda6acc2ff")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
